6941a81e773c1de9d741557526d71a9063a62fea
Aditya P Singh <adityapi3.14@gmail.com>
2024-01-01 10:37:21 +0530

    feat: Setup scanning logic for one-two character lexemes

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -32,10 +32,51 @@ public class Scanner{
 
     private void scanToken(){
         char c = advance();
+        switch(c){
+            // Single-character tokens except Slash (treat comments)
+            case '(': addToken(TokenType.LEFT_PAREN); break;
+            case ')': addToken(TokenType.RIGHT_PAREN); break;
+            case '{': addToken(TokenType.RIGHT_BRACE); break;
+            case '}': addToken(TokenType.RIGHT_BRACE); break;
+            case ',': addToken(TokenType.COMMA); break;
+            case '.': addToken(TokenType.DOT); break;
+            case '-': addToken(TokenType.MINUS); break;
+            case '+': addToken(TokenType.PLUS); break;
+            case ';': addToken(TokenType.SEMICOLON); break;
+            // One or two character tokens
+            case '!':
+                      addToken((match('=')) ? TokenType.BANG_EQUAL : TokenType.BANG); break;
+            case '=':
+                      addToken((match('=')) ? TokenType.EQUAL_EQUAL : TokenType.EQUAL); break;
+            case '>':
+                      addToken((match('=')) ? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
+            case '<':
+                      addToken((match('=')) ? TokenType.LESS_EQUAL : TokenType.LESS); break;
+        }
+    }
+    
+    private void addToken(TokenType type){
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal){
+        tokens.add(new Token(type, source.substring(start, current), literal, line));
     }
 
     private char advance(){
         return source.charAt(current++);
+    }
+
+    private char peek(){
+        return source.charAt(current);
+    }
+
+    private boolean match(char expected){
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
     }
 }
 
