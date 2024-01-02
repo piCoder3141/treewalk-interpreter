714fa0662ea3d282c1b6e1477fb1d68be073231b
Aditya P Singh <adityapi3.14@gmail.com>
2024-01-02 10:38:55 +0530

    feat: Add error handling framework.

--- lox/Lox.java
+++ lox/Lox.java
@@ -8,6 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Lox{
+    static boolean hadError = false;
     public static void main(String[] args) throws IOException {
         if (args.length > 1){
             System.out.println("Usage: jlox [script]");
@@ -22,6 +23,8 @@ public class Lox{
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+
+        if (hadError) System.exit(65);
     }
 
     private static void runPrompt() throws IOException {
@@ -33,10 +36,20 @@ public class Lox{
             String line = reader.readLine();
             if (line == null) break;
             run(line);
+            hadError = false;
         }
     }
 
     private static void run(String source){
         Scanner scanner = new Scanner(source);
+    }
+
+    static void error(int line, String message){
+        report (line, "", message);
+    }
+
+    private static void report(int line, String where, String message){
+        System.out.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
     }
 }
